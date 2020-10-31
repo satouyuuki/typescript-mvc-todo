a2f3e156b96bffd94e3c3a0f64cd692d6818d91b
satoyuki <yuki01377@gmail.com>
2020-10-31 10:17:04 +0900

    dispatcher追加

--- app.js
+++ app.js
@@ -1,8 +1,66 @@
 /**
+ * イベントディスパッチャ(Modelの監視)
+ */
+class EventDispatcher {
+    constructor() {
+        console.log(this);
+        this.listeners = {};
+    }
+    /**
+     * イベントリスナーの追加
+     * @param type イベントタイプ
+     * @param callback
+     */
+    addEventListener(type, callback) {
+        if (!this.listeners[type]) {
+            this.listeners[type] = [];
+        }
+        this.listeners[type].push(callback);
+    }
+    /**
+     * イベントリスナーの削除
+     * @param type イベントタイプ
+     * @param callback
+     */
+    removeEventListener(type, callback) {
+        for (let i = 0; i < this.listeners[type]; i++) {
+            if (this.listeners[type][i] === callback) {
+                this.listeners[type].splice(i, 1);
+            }
+        }
+    }
+    /**
+     * イベントリスナーのクリア
+     */
+    clearEventListener() {
+        this.listeners = {};
+    }
+    /**
+     * ディスパッチの実行
+     * @param event 引数(type: イベントタイプ, [arts]: 任意)
+     */
+    dispatch(event) {
+        if (this.listeners[event.type]) {
+            for (let listener in this.listeners[event.type]) {
+                this.listeners[event.type][listener].apply(this.listeners, arguments);
+            }
+        }
+    }
+}
+/**
+ * Modelクラス
+ */
+class TodoModel extends EventDispatcher {
+    constructor() {
+        super();
+    }
+}
+/**
  * APPクラス(Main)
  */
 class App {
     constructor() {
+        const model = new TodoModel();
     }
 }
 window.onload = () => {
--- app.ts
+++ app.ts
@@ -1,9 +1,75 @@
 /**
+ * イベントディスパッチャ(Modelの監視)
+ */
+class EventDispatcher {
+  'use strict';
+  listeners: any;
+
+  constructor() {
+    console.log(this);
+    this.listeners = {};
+  }
+
+  /**
+   * イベントリスナーの追加
+   * @param type イベントタイプ
+   * @param callback
+   */
+  addEventListener(type: string, callback: Function): void {
+    if (!this.listeners[type]) {
+      this.listeners[type] = []
+    }
+    this.listeners[type].push(callback);
+  }
+
+  /**
+   * イベントリスナーの削除
+   * @param type イベントタイプ
+   * @param callback
+   */
+  removeEventListener(type: string, callback: Function): void {
+    for (let i = 0; i < this.listeners[type]; i++) {
+      if (this.listeners[type][i] === callback) {
+        this.listeners[type].splice(i, 1);
+      }
+    }
+  }
+  /**
+   * イベントリスナーのクリア
+   */
+  clearEventListener(): void {
+    this.listeners = {};
+  }
+  /**
+   * ディスパッチの実行
+   * @param event 引数(type: イベントタイプ, [arts]: 任意)
+   */
+  dispatch(event): void {
+    if (this.listeners[event.type]) {
+      for (let listener in this.listeners[event.type]) {
+        this.listeners[event.type][listener].apply(this.listeners, arguments);
+      }
+    }
+  }
+}
+
+/**
+ * Modelクラス
+ */
+class TodoModel extends EventDispatcher {
+  'use strict';
+  constructor() {
+    super();
+  }
+}
+
+/**
  * APPクラス(Main)
  */
 class App {
   'use strict';
   constructor() {
+    const model = new TodoModel();
   }
 }
 window.onload = () => {
